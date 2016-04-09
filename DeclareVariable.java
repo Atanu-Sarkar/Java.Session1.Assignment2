90ea83a2735e223195873d363abf0a5f3488ee13
Atanu-Sarkar <atanu.noida@gmail.com>
2016-04-09 18:37:07 +0530

    Variable declaration & assignment
    Java Session 1 Assignment2

---
+++ DeclareVariable.java
@@ -0,0 +1,11 @@
+package com.acadgild.session.one;
+
+public class DeclareVariable {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b=4,c;
+		}
+
+}
